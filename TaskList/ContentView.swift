9bfc0cf29aa0f55bdab8101ecbfcc027367a0516
Tissiana Alves <altissiana@gmail.com>
2020-06-29 11:47:36 -0600

    implemented array subscript and got tasks to display on the screen

--- TaskList/ContentView.swift
+++ TaskList/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         List(taskStore.tasks.indices) { index in
-            Text("Hello, World \(index)")
+            Text(self.taskStore.tasks[index].name)
         }
     }
 }
