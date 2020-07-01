f32ebfcf96f30949fee57bf3231c7780d6818c05
Tissiana Alves <altissiana@gmail.com>
2020-07-01 10:39:44 -0600

    added onmove method on the rows

--- TaskList/Views/ContentView.swift
+++ TaskList/Views/ContentView.swift
@@ -19,12 +19,19 @@ struct ContentView: View {
                 ForEach(taskStore.tasks) { task in
                     Text(task.name)
                 }
+                .onMove { sourceIndices, destinationIndex in
+                    self.taskStore.tasks.move(
+                        fromOffsets: sourceIndices,
+                        toOffset: destinationIndex)
+                    
+                }
                 .onDelete { indexSet in
                     self.taskStore.tasks.remove(atOffsets: indexSet)
                 }
             }
             .navigationBarTitle("Tasks")
             .navigationBarItems(
+                leading: EditButton(),
                 trailing:
                 Button(action: { self.modalIsPresented = true }) {
                     Image(systemName: "plus")
