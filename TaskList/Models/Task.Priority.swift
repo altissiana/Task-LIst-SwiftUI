c33c7b128abbd336c7f6208685075e3bb0b1cc32
Tissiana Alves <altissiana@gmail.com>
2020-07-01 11:55:51 -0600

    added Task.Priority

---
+++ TaskList/Models/Task.Priority.swift
@@ -0,0 +1,13 @@
+//
+//  Task.Priority.swift
+//  TaskList
+//
+//  Created by Tissiana Alves on 7/1/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+extension Task {
+    enum Priority {
+        case no, low, medium, high
+    }
+}
---
+++ TaskList/Models/TaskStore.PrioritizedTasks.swift
@@ -0,0 +1,14 @@
+//
+//  TaskStore.PrioritizedTasks.swift
+//  TaskList
+//
+//  Created by Tissiana Alves on 7/1/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+extension TaskStore {
+    struct PrioritizedTasks {
+        let priority: Task.Priority
+        var tasks: [Task]
+    }
+}
